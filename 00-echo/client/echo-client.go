d25518e25fe9e06f5c122447e8bccbd48c39e882
Ronald Fung <rfung.dev@icloud.com>
2024-08-05 21:03:58 +0100

    Modified echo-client.go to take message from argument

--- 00-echo/client/echo-client.go
+++ 00-echo/client/echo-client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 )
 
 const (
@@ -11,6 +12,12 @@ const (
 )
 
 func main() {
+	if len(os.Args[1:]) != 1 {
+		fmt.Println(("Usage: echo-client 'Your message comes here!'"))
+		os.Exit(1)
+	}
+	echo_msg := os.Args[1]
+
 	// Establish connection
 	conn, err := net.Dial(SERVER_TYPE, ":"+SERVER_PORT)
 	if err != nil {
@@ -19,7 +26,7 @@ func main() {
 	defer conn.Close()
 
 	// Write to server
-	_, errWrite := conn.Write([]byte("Hello, World!"))
+	_, errWrite := conn.Write([]byte(echo_msg))
 	if errWrite != nil {
 		fmt.Println(errWrite)
 	}
